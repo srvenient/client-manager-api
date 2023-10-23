1846aa806f9b5348d7806f57c9c2441b4b1eec93
SrVenient <srvenient@gmail.com>
2023-10-23 16:33:01 -0500

    refactor: the packages were renamed

--- core/src/main/java/team/empyte/alliance/core/controller/package-info.java
+++ core/src/main/java/team/empyte/alliance/core/controller/package-info.java
@@ -1,4 +1,4 @@
 /**
  * This package contains the controllers for the client.
  */
-package team.empyte.alliance.core.client.controller;
+package team.empyte.alliance.core.controller;
---
+++ core/src/main/java/team/empyte/alliance/core/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains utility classes.
+ */
+package team.empyte.alliance.core.util;
